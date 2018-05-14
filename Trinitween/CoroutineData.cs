0543c765f1cdbbd3c3f6d88d8b20da314a61be96
BlitzLomi <39267791+BlitzLomi@users.noreply.github.com>
2018-05-14 17:26:32 +0200

    Original Commit

---
+++ Trinitween/CoroutineData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Trinitween.Coroutines
+{
+    class CoTrT
+    {
+        public static IEnumerator SlideValue(Slider slider, float newValue, float smooth)
+        {
+            yield return null;
+            while (slider.value.ToString("F3") != newValue.ToString("F3"))
+            {
+                slider.value = Mathf.Lerp(slider.value, newValue, smooth);
+                yield return new WaitForSecondsRealtime(Time.deltaTime);
+            }
+        }
+        public static IEnumerator Move(Transform transform, Vector3 newValue, float smooth)
+        {
+            while (transform.position.ToString("F2") != newValue.ToString("F2"))
+            {
+                Debug.Log("AAAAAA");
+                transform.position = Vector3.Lerp(transform.position, newValue, smooth);
+                yield return new WaitForSecondsRealtime(Time.deltaTime);
+            }
+			GameObject.Destroy(transform.GetComponent<EmptyScript>());
+        }
+    }
+}
---
+++ Trinitween/Trinitween.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using Trinitween.Threads;
+using Trinitween.Coroutines;
+
+
+namespace Trinitween
+{
+    public static class ShortcutExtensions
+    {
+        public static void TritSlideValue(this Slider slider, float newValue, float smooth = .1f)
+        {
+            slider.StartCoroutine(CoTrT.SlideValue(slider, newValue, smooth));
+        }
+
+        public static void TritMove(this Transform transform, Vector3 newValue, float smooth = .1f)
+        {
+            /*ThreadMoveHandle tmh = new ThreadMoveHandle(transform, newValue, smooth);
+            Thread t = new Thread(new ThreadStart(tmh.ThreadMove));
+            t.Start();*/
+            Debug.Log("BBBBB");
+			transform.GetOrAddComponent<EmptyScript>().StartCoroutine(CoTrT.Move(transform, newValue, smooth));
+            //CoTrT.Move(transform, newValue, smooth);
+            //MonoBehaviour mono = new MonoBehaviour();
+            //mono.StartCoroutine();
+        }
+
+    }
+
+
+
+
+}
