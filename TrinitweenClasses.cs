cc2185beb2396a6dc2c2469fc17b16e9b686eeaf
BlitzLomi <39267791+BlitzLomi@users.noreply.github.com>
2018-05-31 13:55:24 +0200

    Update Fix sync tweens

---
+++ TrinitweenClasses.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Trinitween.InternalData
+{
+    public class TriTween
+    {
+        public float progress;
+        public bool isDurationBased;
+        public float smooth;
+        public SmoothType smoothType;
+    }
+
+}
+namespace Trinitween
+{
+    public enum SmoothType
+    {
+        Linear,
+        EaseOut
+    }
+
+}
